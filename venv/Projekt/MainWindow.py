2d02630fd5bbe1d3cd691c277d2db399a134a08b
lzwierz <62058336+lzwierz@users.noreply.github.com>
2020-12-06 19:51:43 +0100

    Update MainWindow.py

--- venv/Projekt/MainWindow.py
+++ venv/Projekt/MainWindow.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(858, 604)
+        MainWindow.setFixedSize(900, 604)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
